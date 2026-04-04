72d88fc0d769046367f39628d891f8e2a514e099
Miles <44444826+MilesLabrador@users.noreply.github.com>
2026-04-03 20:41:00 -0700

    Update version and add same functionality from Gantt view to List view

--- FieldService/zebra_stripe_schedule_board.user.js
+++ FieldService/zebra_stripe_schedule_board.user.js
@@ -3,7 +3,7 @@
 // @namespace    Violentmonkey Scripts
 // @match        https://*.dynamics.com/main.aspx*
 // @grant        none
-// @version      1.2
+// @version      1.3
 // @author       Miles Labrador
 // @description  Zebra stripes for the D365 Field Service Schedule Board timeline grid
 // @license MIT
@@ -11,8 +11,7 @@
 // @updateURL https://update.greasyfork.org/scripts/572448/D365%20Schedule%20Board%20Zebra%20Stripes.meta.js
 // ==/UserScript==
 
-function injectZebraStripes() {
-  // Only run if the ScheduleBoard iframe is present
+function injectStripes() {
   var iframes = document.querySelectorAll('iframe');
   var found = false;
   for (var i = 0; i < iframes.length; i++) {
@@ -27,35 +26,45 @@ function injectZebraStripes() {
 
   for (var i = 0; i < iframes.length; i++) {
     try {
-      var count = iframes[i].contentDocument.querySelectorAll('.b-grid-row').length;
-      if (count > 0) {
-        var doc = iframes[i].contentDocument;
-        if (doc.getElementById('zebra-style')) return;
+      var doc = iframes[i].contentDocument;
+      var gridRows = doc.querySelectorAll('.b-grid-row').length;
+      var listRows = doc.querySelectorAll('.ms-GroupHeader').length;
 
-        var oddRows = [];
-        var evenRows = [];
-        for (var n = 0; n < 200; n++) {
-          var selector = `.b-grid-row:not([data-id*="_generatedt_"])[data-index="${n}"] .b-grid-cell`;
-          if (n % 2 === 0) {
-            oddRows.push(selector);
-          } else {
-            evenRows.push(selector);
+      if (gridRows > 0 || listRows > 0) {
+
+        // Timeline grid stripes
+        if (gridRows > 0 && !doc.getElementById('zebra-style')) {
+          var oddRows = [];
+          var evenRows = [];
+          for (var n = 0; n < 200; n++) {
+            var selector = `.b-grid-row:not([data-id*="_generatedt_"])[data-index="${n}"] .b-grid-cell`;
+            if (n % 2 === 0) {
+              oddRows.push(selector);
+            } else {
+              evenRows.push(selector);
+            }
           }
+          var css = `
+            ${oddRows.join(',\n')} {
+              background-color: #daeaf7 !important;
+            }
+            ${evenRows.join(',\n')} {
+              background-color: #ffffff !important;
+            }
+          `;
+          var style = doc.createElement('style');
+          style.id = 'zebra-style';
+          style.innerHTML = css;
+          doc.head.appendChild(style);
         }
 
-        var css = `
-          ${oddRows.join(',\n')} {
-            background-color: #daeaf7 !important;
-          }
-          ${evenRows.join(',\n')} {
-            background-color: #ffffff !important;
-          }
-        `;
+        // List view stripes
+        if (listRows > 0) {
+          doc.querySelectorAll('.ms-GroupHeader').forEach(function(row, i) {
+            row.style.backgroundColor = i % 2 === 0 ? '#daeaf7' : '#ffffff';
+          });
+        }
 
-        var style = doc.createElement('style');
-        style.id = 'zebra-style';
-        style.innerHTML = css;
-        doc.head.appendChild(style);
       }
     } catch(e) {}
   }
@@ -63,7 +72,7 @@ function injectZebraStripes() {
 
 var attempts = 0;
 var interval = setInterval(function() {
-  injectZebraStripes();
+  injectStripes();
   attempts++;
   if (attempts > 30) clearInterval(interval);
 }, 1000);
